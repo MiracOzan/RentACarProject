0a3faf3e68d2fdee0ae0897becd022626a850ce6
Miraç Ozan T <m.ozan.tosun@gmail.com>
2022-08-15 22:27:44 +0300

    Add project files.

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+
+        }
+
+        public Brand(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
